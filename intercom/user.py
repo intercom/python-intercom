85e05d98cc7d82c1f7c7747e4d2b51d611fa7cbf
John Keyes <john@keyes.ie>
2012-03-25 23:39:07 +0100

    Added docstrings and doctests
    A bunch of example code in there now. Some local pylint disables to
    achieve the 10 rating.

--- intercom/user.py
+++ intercom/user.py
@@ -3,6 +3,14 @@
 #
 # License: http://jkeyes.mit-license.org/
 #
+""" User module. 
+
+>>> from intercom import Intercom
+>>> Intercom.app_id = 'dummy-app-id'
+>>> Intercom.api_key = 'dummy-api-key'
+>>> from intercom import User
+
+"""
 
 from . import Intercom
 from . import from_timestamp_property
@@ -11,48 +19,91 @@ from . import to_timestamp_property
 import numbers
 
 class UserId(dict):
+    """ Base class for objects that required user_id and email properties. """
 
     @property
     def user_id(self):
+        """ Returns the user_id. """
         return dict.get(self, 'user_id', None)
 
     @user_id.setter
     def user_id(self, user_id):
+        """ Sets the user_id. """
         self['user_id'] = user_id
 
     @property
     def email(self):
+        """ Returns the email address. """
         return dict.get(self, 'email', None)
 
     @email.setter
     def email(self, email):
+        """ Sets the email address. """
         self['email'] = email
 
-class User(UserId):
-    """ Object representing http://docs.intercom.io/#UserData) """
+class User(UserId): # pylint: disable=R0904
+    """ Object representing http://docs.intercom.io/#UserData).  """
 
     attributes = ('user_id', 'email', 'name', 'created_at', 'custom_data',
             'last_seen_ip', 'last_seen_user_agent')
 
     @classmethod
     def find(cls, user_id=None, email=None):
+        """ Find a user by email or user_id.
+
+        >>> user = User.find(email="somebody@example.com")
+        >>> user.user_id
+        u'123'
+        >>> user.name
+        u'Somebody'
+        >>> user = User.find(user_id=123)
+        >>> user.email
+        u'bob@example.com'
+        >>> user.name
+        u'Bob'
+
+        """
         resp = Intercom.get_user(user_id=user_id, email=email)
         return cls(resp)
 
     @classmethod
     def find_by_email(cls, email):
+        """ Find a user by email.
+
+        >>> user = User.find_by_email("somebody@example.com")
+        >>> user.user_id
+        u'123'
+        >>> user.name
+        u'Somebody'
+
+        """
         resp = Intercom.get_user(email=email)
         return cls(resp)
 
     @classmethod
     def find_by_user_id(cls, user_id):
+        """ Find a user by user_id.
+
+        >>> user = User.find(user_id=123)
+        >>> user.email
+        u'bob@example.com'
+        >>> user.name
+        u'Bob'
+
+        """
         resp = Intercom.get_user(user_id=user_id)
         return cls(resp)
 
     @classmethod
     def create(cls, user_id=None, email=None, name=None, created_at=None, 
             custom_data=None, last_seen_ip=None, last_seen_user_agent=None):
+        """ Create or update a user.
 
+        >>> user = User.create(email="somebody@example.com")
+        >>> user.name
+        u'Somebody'
+
+        """
         resp = Intercom.create_user(user_id=user_id, email=email, name=name, 
                 created_at=created_at, custom_data=custom_data, 
                 last_seen_ip=last_seen_ip, last_seen_user_agent=last_seen_user_agent)
@@ -60,10 +111,28 @@ class User(UserId):
 
     @classmethod
     def all(cls):
-        resp = Intercom().get_users()
+        """ Return all of the Users. 
+
+        >>> users = User.all()
+        >>> len(users)
+        3
+        >>> users[0].email
+        u'first.user@example.com'
+
+        """
+        resp = Intercom.get_users()
         return [cls(u) for u in resp['users']]
 
     def save(self):
+        """ Creates or updates a User. 
+
+        >>> user = User()
+        >>> user.email = "somebody@example.com"
+        >>> user.save()
+        >>> user.name
+        u'Somebody'
+
+        """
         attrs = {}
         for key in User.attributes:
             value = dict.get(self, key) 
@@ -74,59 +143,96 @@ class User(UserId):
 
     @property
     def name(self):
+        """ Returns the name e.g. Joe Bloggs. """
         return dict.get(self, 'name', None)
 
     @name.setter
     def name(self, name):
+        """ Sets the name. """
         self['name'] = name
 
     @property
     def last_seen_ip(self):
+        """ Returns the last seen IP address. """
         return dict.get(self, 'last_seen_ip', None)
 
     @last_seen_ip.setter
     def last_seen_ip(self, last_seen_ip):
+        """ Sets the last seen IP address. """
         self['last_seen_ip'] = last_seen_ip
 
     @property
     def last_seen_user_agent(self):
+        """ Returns the last seen User Agent. """
         return dict.get(self, 'last_seen_user_agent', None)
 
     @last_seen_user_agent.setter
     def last_seen_user_agent(self, last_seen_user_agent):
+        """ Sets the last seen User Agent. """
         self['last_seen_user_agent'] = last_seen_user_agent
 
     @property
     def relationship_score(self):
+        """ Returns the relationship score. """
         return dict.get(self, 'relationship_score', None)
 
     @property
     def session_count(self):
+        """ Returns how many sessions this User has used on your application. """
         return dict.get(self, 'session_count', None)
 
     @property
     @from_timestamp_property
     def last_impression_at(self):
+        """ Returns the datetime this User last used your application. """
         return dict.get(self, 'last_impression_at', None)
 
     @property
     @from_timestamp_property
     def created_at(self):
+        """ Returns the datetime this User started using your application. """
         return dict.get(self, 'created_at', None)
 
     @created_at.setter
     @to_timestamp_property
     def created_at(self, value):
+        """ Sets the timestamp when this User started using your application. """
         self['created_at'] = value
 
     @property
     def social_profiles(self):
+        """ Returns a list of SocialProfile objects for this User. 
+
+        >>> users = User.all()
+        >>> social_profiles = users[0].social_profiles
+        >>> len(social_profiles)
+        2
+        >>> type(social_profiles[0])
+        <class 'intercom.user.SocialProfile'>
+        >>> social_profiles[0].type
+        u'twitter'
+        >>> social_profiles[0].url
+        u'http://twitter.com/abc'
+
+        """
         profiles = dict.get(self, 'social_profiles', None)
         if profiles:
             return [SocialProfile(**p) for p in profiles]
 
     @property
     def location_data(self):
+        """ Returns a LocationData object for this User. 
+
+        >>> users = User.all()
+        >>> location_data = users[0].location_data
+        >>> type(location_data)
+        <class 'intercom.user.LocationData'>
+        >>> location_data.country_name
+        u'Chile'
+        >>> location_data.city_name
+        u'Santiago'
+
+        """
         data = dict.get(self, 'location_data', None)
         if not isinstance(data, LocationData):
             data = LocationData(data)
@@ -135,6 +241,16 @@ class User(UserId):
 
     @property
     def custom_data(self):
+        """ Returns a CustomData object for this User.
+
+        >>> users = User.all()
+        >>> custom_data = users[0].custom_data
+        >>> type(custom_data)
+        <class 'intercom.user.CustomData'>
+        >>> custom_data['monthly_spend']
+        155.5
+
+        """
         data = dict.get(self, 'custom_data', None)
         if not isinstance(data, CustomData):
             data = CustomData(data)
@@ -143,13 +259,41 @@ class User(UserId):
 
     @custom_data.setter
     def custom_data(self, custom_data):
+        """ Sets the CustomData for this User. 
+
+        >>> user = User(email="somebody@example.com")
+        >>> user.custom_data = { 'max_monthly_spend': 200 }
+        >>> type(custom_data)
+        <class 'intercom.user.CustomData'>
+        >>> user.save()
+        >>> len(user.custom_data)
+        1
+        >>> user.custom_data['avg_monthly_spend'] = 102
+        >>> user.save()
+        >>> len(user.custom_data)
+        2
+
+        """
         if not isinstance(custom_data, CustomData):
             custom_data = CustomData(custom_data)
         self['custom_data'] = custom_data
 
 class CustomData(dict):
     """ A dict that limits keys to strings, and values to real numbers
-    and strings. """
+    and strings. 
+
+    >>> from intercom.user import CustomData
+    >>> data = CustomData()
+    >>> data['a_dict'] = {}
+    Traceback (most recent call last):
+        ...
+    ValueError: custom data only allows string and real number values
+    >>> data[1] = "a string"
+    Traceback (most recent call last):
+        ...
+    ValueError: custom data only allows string keys
+
+    """
 
     def __setitem__(self, key, value):
         """ Limits the keys and values. """
@@ -159,70 +303,105 @@ class CustomData(dict):
             raise ValueError("custom data only allows string keys")
         super(CustomData, self).__setitem__(key, value)
 
-class SocialProfile(dict):
-    """ Object representing http://docs.intercom.io/#SocialProfiles) """
+class SocialProfile(dict): # pylint: disable=R0921
+    """ Object representing http://docs.intercom.io/#SocialProfiles) 
+
+    This object is read-only, and to hint at this __setitem__ is disabled.
+
+    >>> from intercom.user import SocialProfile
+    >>> profile = SocialProfile(type=u'twitter')
+    >>> profile.type
+    u'twitter'
+    >>> profile['type'] = 'facebook'
+    Traceback (most recent call last):
+        ...
+    NotImplementedError
+
+    """
 
     @property
     def type(self):
-        """ type e.g. twitter, facebook, flickr, etc. """
+        """ The type e.g. twitter, facebook, flickr, etc. """
         return self.get('type', None)
 
     @property
-    def id(self):
-        """ id """
+    def id(self): # pylint: disable=C0103
+        """ The id """
         return self.get('id', None)
 
     @property
     def url(self):
-        """ profile url """
+        """ The profile url e.g. http://twitter.com/somebody """
         return self.get('url', None)
 
     @property
     def username(self):
-        """ username """
+        """ The profile username e.g. somebody """
         return self.get('username', None)
 
     def __setitem__(self, key, value):
         """ Do not allow items to be set. """
         raise NotImplementedError
 
-class LocationData(dict):
-    """ Object representing a user's location data """
+class LocationData(dict): # pylint: disable=R0921
+    """ Object representing a user's location data
+
+    This object is read-only, and to hint at this __setitem__ is disabled.
+
+    >>> from intercom.user import SocialProfile
+    >>> profile = SocialProfile(type=u'twitter')
+    >>> profile.type
+    u'twitter'
+    >>> profile['type'] = 'facebook'
+    Traceback (most recent call last):
+        ...
+    NotImplementedError
+
+    """
 
     @property
     def city_name(self):
+        """ The city name. """
         return self.get('city_name', None)
 
     @property
     def continent_code(self):
+        """ The continent code. """
         return self.get('continent_code', None)
 
     @property
     def country_name(self):
+        """ The country name. """
         return self.get('country_name', None)
 
     @property
     def latitude(self):
+        """ Latitude. """
         return self.get('latitude', None)
 
     @property
     def longitude(self):
+        """ Longitude. """
         return self.get('longitude', None)
 
     @property
     def postal_code(self):
+        """ The postal code. """
         return self.get('postal_code', None)
 
     @property
     def region_name(self):
+        """ The region name. """
         return self.get('region_name', None)
 
     @property
     def timezone(self):
+        """ The timezone. """
         return self.get('timezone', None)
 
     @property
     def country_code(self):
+        """ The country code. """
         return self.get('country_code', None)
 
     def __setitem__(self, key, value):
