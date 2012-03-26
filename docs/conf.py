bcb2119ff156aa8c0bdeb31bdf607ad05fc690ab
John Keyes <john@keyes.ie>
2012-03-26 10:53:29 +0100

    Bumped to 0.2.2

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ copyright = u'2012, John Keyes'
 # built documents.
 #
 # The short X.Y version.
-version = '0.2.1'
+version = '0.2.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
