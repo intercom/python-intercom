6ad7b3213222218eea130d5e2f15a4779350e192
John Keyes <johnkeyes@gmail.com>
2013-02-25 23:13:29 +0000

    Another version string. Need to centralize these.

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ copyright = u'2012, John Keyes'
 # built documents.
 #
 # The short X.Y version.
-version = '0.2.3'
+version = '0.2.4'
 # The full version, including alpha/beta/rc tags.
 release = version
 
