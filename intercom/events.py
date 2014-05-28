c510bde9de426e1ab32c440da66c90e7adc94bab
Martin-Zack Mekkaoui <martin@mekkaoui.fr>
2014-05-28 16:55:16 +0200

    Typo

--- intercom/events.py
+++ intercom/events.py
@@ -13,5 +13,5 @@ class Event(dict):
 
     @classmethod
     def create(cls, event_name=None, user_id=None, email=None, metadata=None):
-        resp = Intercom.create_event(event_name=name, user_id=user_id, email=email, metadata=metadata)
+        resp = Intercom.create_event(event_name=event_name, user_id=user_id, email=email, metadata=metadata)
         return Event(resp)
