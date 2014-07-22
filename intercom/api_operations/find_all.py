ecd5c618ad598fb35b1b37bef5305be3ace7b10c
John Keyes <johnkeyes@gmail.com>
2014-07-23 00:40:08 +0100

    Moving FindAll into it's own module. Moving CollectionProxy into it's own module.

---
+++ intercom/api_operations/find_all.py
@@ -0,0 +1,16 @@
+from intercom import utils
+from intercom.collection_proxy import CollectionProxy
+
+
+class FindAll(object):
+
+    @classmethod
+    def find_all(cls, **params):
+        collection = utils.resource_class_to_collection_name(cls)
+        print "find_all %s in %s" % (params, collection)
+        if 'id' in params and 'type' not in params:
+            finder_url = "/%s/%s" % (collection, params['id'])
+        else:
+            finder_url = "/%s" % (collection)
+        finder_params = params
+        return CollectionProxy(cls, collection, finder_url, finder_params)
--- intercom/user.py
+++ intercom/user.py
@@ -11,7 +11,9 @@ import datetime
 import time
 import types
 from intercom.api_operations.find import Find
+from intercom.api_operations.find_all import FindAll
 from intercom.api_operations.save import Save
+from intercom.collection_proxy import CollectionProxy
 
 
 class ArgumentError(ValueError):
@@ -224,96 +226,6 @@ def create_class_instance(class_name):
     CLASS_REGISTRY[class_name] = dyncls
     return dyncls
 
-
-class CollectionProxy(object):
-
-    def __init__(self, cls, collection, finder_url, finder_params={}):
-        # needed to create class instances of the resource
-        self.collection_cls = cls
-
-        # needed to reference the collection in the response
-        self.collection = collection
-
-        # the original URL to retrieve the resources
-        self.finder_url = finder_url
-
-        # the params to filter the resources
-        self.finder_params = finder_params
-
-        # an iterator over the resources found in the response
-        self.resources = None
-
-        # a link to the next page of results
-        self.next_page = None
-
-
-    def __iter__(self):
-        return self
-
-    def next(self):
-        if self.resources is None:
-            # get the first page of results
-            self.get_first_page()
-
-        # try to get a resource if there are no more in the 
-        # current resource iterator (StopIteration is raised)
-        # try to get the next page of results first
-        try:
-            resource = self.resources.next()
-        except StopIteration:
-            self.get_next_page()
-            resource = self.resources.next()
-
-        instance = self.collection_cls(**resource)
-        return instance
-
-    def get_first_page(self):
-        # get the first page of results
-        return self.get_page(self.finder_url, self.finder_params)
-
-    def get_next_page(self):
-        # get the next page of results
-        return self.get_page(self.next_page)
-
-    def get_page(self, url, params={}):
-        # get a page of results
-
-        # if there is no url stop iterating
-        if url is None:
-            raise StopIteration
-
-        response = Intercom.get(url, **params)
-        collection = response[self.collection]
-        # if there are no resources in the response stop iterating
-        if collection is None:
-            raise StopIteration
-
-        # create the resource iterator
-        self.resources = iter(collection)
-        # grab the next page URL if one exists
-        self.next_page = self.extract_next_link(response)
-
-    def paging_info_present(self, response):
-        return 'pages' in response and 'type' in response['pages']
-
-    def extract_next_link(self, response):
-        if self.paging_info_present(response):
-            paging_info = response["pages"]
-            return paging_info["next"]
-
-
-class FindAll(object):
-
-    @classmethod
-    def find_all(cls, **params):
-        collection = utils.resource_class_to_collection_name(cls)
-        print "find_all %s in %s" % (params, collection)
-        if 'id' in params and 'type' not in params:
-            finder_url = "/%s/%s" % (collection, params['id'])
-        else:
-            finder_url = "/%s" % (collection)
-        finder_params = params
-        return CollectionProxy(cls, collection, finder_url, finder_params)
 
 
 class IncrementableAttributes(object):
