8c3140f01a9314992fc64787c72eb3304ef366c8
John Keyes <john@keyes.ie>
2015-03-18 09:43:58 +0000

    Marking some lines as noqa, and some tidy-up.

--- tests/unit/notification_spec.py
+++ tests/unit/notification_spec.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
+
 from describe import expect
 from intercom.notification import Notification
 from intercom.utils import create_class_instance
-from tests.unit import test_user_notification
 from tests.unit import test_conversation_notification
+from tests.unit import test_user_notification
 
 
 class DescribeIntercomNotification:
@@ -13,7 +15,7 @@ class DescribeIntercomNotification:
 
     def it_returns_correct_model_type_for_user(self):
         payload = Notification(**test_user_notification)
-        User = create_class_instance('User')
+        User = create_class_instance('User')  # noqa
 
         expect(payload.model).to.be_instance_of(User.__class__)
         expect(payload.model_type) == User.__class__
@@ -28,12 +30,12 @@ class DescribeIntercomNotification:
         expect(payload.model).to.be_instance_of(User.__class__)
 
     def it_returns_instance_of_conversation(self):
-        Conversation = create_class_instance('Conversation')
+        Conversation = create_class_instance('Conversation')  # noqa
         payload = Notification(**test_conversation_notification)
         expect(payload.model).to.be_instance_of(Conversation.__class__)
 
     def it_returns_correct_model_type_for_conversation(self):
-        Conversation = create_class_instance('Conversation')
+        Conversation = create_class_instance('Conversation')  # noqa
         payload = Notification(**test_conversation_notification)
         expect(payload.model_type) == Conversation.__class__
 
@@ -42,7 +44,7 @@ class DescribeIntercomNotification:
         expect(payload.topic) == "conversation.user.created"
 
     def it_returns_inner_user_object_for_conversation(self):
-        User = create_class_instance('User')
+        User = create_class_instance('User')  # noqa
         payload = Notification(**test_conversation_notification)
         expect(payload.model.user).to.be_instance_of(User.__class__)
 
