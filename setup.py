9d5293ebdcd665492833d3219afb495576eeb34d
Grant McConnaughey <grantmcconnaughey@gmail.com>
2015-10-13 15:22:16 -0500

    Add python classifiers

--- setup.py
+++ setup.py
@@ -28,7 +28,12 @@ setup(
     license="MIT License",
     url="http://github.com/jkeyes/python-intercom",
     keywords='Intercom crm python',
-    classifiers=[],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
     packages=find_packages(),
     include_package_data=True,
     install_requires=["requests", "inflection", "certifi", "six"],
