bce304f2c5f0806fa29a3c0c2a9a6138a93c7abf
John Keyes <john@keyes.ie>
2016-01-18 00:50:03 +0000

    Using new client approach for the issues tests in integration.

--- tests/integration/issues/test_73.py
+++ tests/integration/issues/test_73.py
@@ -5,30 +5,31 @@ How do I record when a User has started a new session?
 
 import os
 import unittest
-from intercom import Intercom
-from intercom import User
 
-Intercom.app_id = os.environ.get('INTERCOM_APP_ID')
-Intercom.app_api_key = os.environ.get('INTERCOM_APP_API_KEY')
+from intercom.client import Client
+
+intercom = Client(
+    os.environ.get('INTERCOM_APP_ID'),
+    os.environ.get('INTERCOM_API_KEY'))
 
 
 class Issue73Test(unittest.TestCase):
 
     def test(self):
-        user = User.create(email='bingo@example.com')
+        user = intercom.users.create(email='bingo@example.com')
         # store current session count
         session_count = user.session_count
 
         # register a new session
         user.new_session = True
-        user.save()
+        intercom.users.save(user)
 
         # count has increased by 1
         self.assertEquals(session_count + 1, user.session_count)
 
         # register a new session
         user.new_session = True
-        user.save()
+        intercom.users.save(user)
 
         # count has increased by 1
         self.assertEquals(session_count + 2, user.session_count)
