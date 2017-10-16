fcdce48c9290716a978be604888a9feb2fa70588
tanasegabriel <tanase.gabriel91@gmail.com>
2017-10-16 19:43:33 +0100

    Added support for finding admin by ID

--- intercom/service/admin.py
+++ intercom/service/admin.py
@@ -2,10 +2,11 @@
 
 from intercom import admin
 from intercom.api_operations.all import All
+from intercom.api_operations.find import Find
 from intercom.service.base_service import BaseService
 
 
-class Admin(BaseService, All):
+class Admin(BaseService, All, Find):
 
     @property
     def collection_class(self):
