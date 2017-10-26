62e5f021c6489876ece0b032858f3afc768c4bb8
lukas-gitl <lukas@getintheloop.ca>
2017-10-26 09:41:58 -0700

    Parameter now singular (e.g. user_id)

--- intercom/api_operations/delete.py
+++ intercom/api_operations/delete.py
@@ -9,9 +9,10 @@ class Delete(object):
 
     def delete(self, obj=None, **kwargs):
         """Delete the specified instance of this resource."""
+        assert obj is not None or kwargs.keys()[0] == "%s_id" % (
+            utils.resource_class_to_name(self.collection_class))
+        obj_id = kwargs.values()[0] if obj is None else obj.id
         collection = utils.resource_class_to_collection_name(
             self.collection_class)
-        assert obj is not None or kwargs.keys()[0] == "%s_id" % collection
-        obj_id = kwargs.values()[0] if obj is None else obj.id
         self.client.delete("/%s/%s" % (collection, obj_id), {})
         return obj
