913ff75784add0ed6c52352f14923a1361f3c773
Michael <michael-k@users.noreply.github.com>
2018-03-07 12:32:44 +0000

    Add python 3.5 and 3.6 to classifiers

--- setup.py
+++ setup.py
@@ -33,6 +33,8 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     packages=find_packages(),
     include_package_data=True,
