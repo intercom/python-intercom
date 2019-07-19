75688bac93d4a146d538b96a79c84fafff96e58b
Kacper <kacper@kacperadler.info>
2019-07-19 10:21:49 +0200

    Added scroll for companies

--- intercom/service/company.py
+++ intercom/service/company.py
@@ -7,12 +7,13 @@ from intercom.api_operations.find import Find
 from intercom.api_operations.find_all import FindAll
 from intercom.api_operations.save import Save
 from intercom.api_operations.load import Load
+from intercom.api_operations.scroll import Scroll
 from intercom.extended_api_operations.users import Users
 from intercom.extended_api_operations.tags import Tags
 from intercom.service.base_service import BaseService
 
 
-class Company(BaseService, All, Delete, Find, FindAll, Save, Load, Users, Tags):
+class Company(BaseService, All, Delete, Find, FindAll, Save, Load, Users, Tags, Scroll):
 
     @property
     def collection_class(self):
