86286b1355d7dcac370bb7e6e4bb314ddfecc267
martin <martin@fauna.eco>
2021-10-05 14:23:24 +0200

    Fix Contact resource  to be PUT

--- intercom/contact.py
+++ intercom/contact.py
@@ -6,9 +6,10 @@ from intercom.traits.incrementable_attributes import IncrementableAttributes
 
 class Contact(Resource, IncrementableAttributes):
 
-    update_verb = 'post'
+    update_verb = 'put'
     identity_vars = ['id', 'email', 'role']
 
     @property
     def flat_store_attributes(self):
         return ['custom_attributes']
+
