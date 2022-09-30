b4eda6b8392f353412b06217e940ea265c7fbff1
Dimosthenis Schizas <dimos@hackthebox.eu>
2022-09-30 15:28:09 +0300

    Fix version

--- intercom/__init__.py
+++ intercom/__init__.py
@@ -8,7 +8,7 @@ from .errors import (ArgumentError, AuthenticationError, BadGatewayError,
                      ServiceUnavailableError, TokenUnauthorizedError,
                      UnexpectedError)
 
-__version__ = '3.1.0'
+__version__ = '4.0.0'
 
 
 RELATED_DOCS_TEXT = "See https://github.com/jkeyes/python-intercom \
